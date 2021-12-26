6f4df6f3450e2f468150d42e37d0e268c2250dcd
Simon <simonlee1125@gmail.com>
2021-12-26 14:24:27 -0500

    Added reset button to clear canvas

--- etchASketch.js
+++ etchASketch.js
@@ -22,7 +22,7 @@ function createGrid() {
     for (i=1; i<257; i++) {
         const box = document.createElement('div');
         box.classList.add('box');
-        box.style.cssText='background: white;'
+        box.textContent = (' ')
         container.appendChild(box);
     }
 }
@@ -33,9 +33,27 @@ const boxes = document.querySelectorAll('.box');
 
 boxes.forEach((box) => { // hovering listeners
     box.addEventListener('mouseover', (e) => {
-        e.target.classList.toggle('hovering');
+        box.style.background='orange';
     });
-    box.addEventListener('mouseout', (e) => {
-        e.target.classList.toggle('hovering')
-    })
+
+    // box.addEventListener('mouseout', (e) => {
+    //     e.target.classList.toggle('hovering');
+    //     box.style.background='white';
+    // })
 })
+
+function clearBoxes() {
+    boxes.forEach((box) => {
+        box.style.background='white'
+    })
+}
+
+const resetButton = document.querySelector('.reset');
+
+resetButton.addEventListener('mouseover', (e) => {
+    resetButton.classList.toggle('hovering');
+})
+resetButton.addEventListener('mouseout', (e) => {
+    resetButton.classList.toggle('hovering');
+})
+resetButton.addEventListener('click', () => {clearBoxes()})
