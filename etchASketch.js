d5584967ae21e29affb6c6303b32d54ed4a74b31
Simon <simonlee1125@gmail.com>
2021-12-26 13:44:31 -0500

    Reworked previous createGrid() function with CSS grid implementation.

--- etchASketch.js
+++ etchASketch.js
@@ -2,19 +2,28 @@ const body = document.querySelector('body');
 
 const container = document.querySelector('.container')
 
+// function createGrid() {
+//     for (i=0; i<16; i++) {
+//         const row = document.createElement('div');
+//         row.classList.add('row');
+//         row.classList.add(`${i}`);
+//         row.style.cssText=''
+//         container.appendChild(row);
+//         for (j=0; j<16; j++) {
+//             const box = document.createElement('div');
+//             box.classList.add('box');
+//             box.style.cssText='height:50px; width: 50px; background-color:black;';
+//             row.appendChild(box);
+//         }
+//     }
+// }
+
 function createGrid() {
-    for (i=0; i<16; i++) {
-        const row = document.createElement('div');
-        row.classList.add('row');
-        row.classList.add(`${i}`);
-        row.style.cssText='display:flex; height: 50px; width: 800px;'
-        container.appendChild(row);
-        for (j=0; j<16; j++) {
-            const box = document.createElement('div');
-            box.classList.add('box');
-            box.style.cssText='width:5vw; background-color:black;';
-            row.appendChild(box);
-        }
+    for (i=1; i<257; i++) {
+        const box = document.createElement('div');
+        box.classList.add('box');
+        box.style.cssText='background:black;'
+        container.appendChild(box);
     }
 }
 
